af7a5cafa5cab6a4d46adb1d38c05be29845b5e3
Afsaneh Mohammadzaheri <afsaneh.mohamadzaheri@gmail.com>
2025-05-06 15:01:04 +0100

    Cleaned app/

--- app/run_predict.py
+++
@@ -1,9 +0,0 @@
-# run_predict.py
-
-from predict_llm import predict_news
-
-if __name__ == "__main__":
-    example_text = "Breaking news: Aliens landed in Paris!"
-    prediction = predict_news(example_text)
-    print(f"Prediction: {prediction}")
-
