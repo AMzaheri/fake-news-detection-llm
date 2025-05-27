6c105dfcff03148e717d09acc439918b1b6e7964
Afsaneh Mohammadzaheri <afsaneh.mohamadzaheri@gmail.com>
2025-05-27 11:11:33 +0100

    Map the raw label to the REAl/FAKE tags

--- app_streamlit/utils.py
+++ app_streamlit/utils.py
@@ -7,7 +7,21 @@ MODEL_ID = "afsanehm/fake-news-detection-llm"
 def load_pipeline():
     return pipeline("text-classification", model=MODEL_ID, tokenizer=MODEL_ID)
 
+#def predict(text):
+#    classifier = load_pipeline()
+#    result = classifier(text)[0]
+#    return result['label']
+
 def predict(text):
     classifier = load_pipeline()
     result = classifier(text)[0]
-    return result['label']
+    raw_label = result["label"]
+
+    label_map = {
+        "LABEL_0": "FAKE",
+        "LABEL_1": "REAL"
+    }
+
+    return label_map.get(raw_label, /
+           raw_label)  # fallback to raw label if unknown
+
